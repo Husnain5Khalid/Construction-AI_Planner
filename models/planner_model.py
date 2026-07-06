b574a728a0af6d0a10f49a8bd3b2fe20c709a2af
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-07-06 15:19:53 +0300

    Updated The Planner Output

--- models/planner_model.py
+++ models/planner_model.py
@@ -1,8 +1,16 @@
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
+'''
+class PlannerDecision(BaseModel):
+    route: str
+    tool_name: Optional[str] = None
+    reason: str
+'''
+#upper coment out funcion follow before step7 in lesson 3. After step 6 it is updated
 
 class PlannerDecision(BaseModel):
     route: str
     tool_name: Optional[str] = None
+    tool_input: dict = Field(default_factory=dict) #default_factory=Whenever a new PlannerDecision object is created, call dict() to make a brand-new empty dictionary.
     reason: str
